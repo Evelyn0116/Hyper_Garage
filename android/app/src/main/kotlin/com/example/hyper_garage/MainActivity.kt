917833acc4e912aacc02deb22473d94c30b10ad6
min <48940875+Evelyn0116@users.noreply.github.com>
2020-11-25 00:20:29 -0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/hyper_garage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hyper_garage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'Config/config.dart';
+import 'Store/storehome.dart';
+import 'Authentication/authentication.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    displaySplash();
+  }
+
+  displaySplash() {
+    Timer(Duration(seconds: 5), () async {
+      if(await HyperGarageApp.auth.currentUser() != null) {
+        Route route = MaterialPageRoute(builder: (_) => StoreHome());
+        Navigator.pushReplacement(context, route);
+      } else {
+        Route route = MaterialPageRoute(builder: (_) => AuthenticScreen());
+        Navigator.pushReplacement(context, route);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                "MM YQF Hyper Garage",
+                style: TextStyle(color: Colors.deepPurple, fontSize: 30)
+
+            ),
+            Image.asset("images/welcome2.jpg"),
+            SizedBox(height: 30.0,),
+
+          ]
+        )
+      )
+    );
+  }
+
+}
+
