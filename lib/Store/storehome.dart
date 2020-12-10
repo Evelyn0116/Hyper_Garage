457f2e2a47ae50f0a1783310d7c870d85c507132
Bambi <yuqifang.school@gmail.com>
2020-12-10 13:40:54 -0800

    test branch

--- lib/Store/storehome.dart
+++ lib/Store/storehome.dart
@@ -10,16 +10,41 @@ class StoreHome extends StatefulWidget {
 }
 
 class _StoreHomeState extends State<StoreHome> {
+  // Widget build(BuildContext context) {
+  //   width = MediaQuery.of(context).size.width;
+  //   return SafeArea(
+  //     child: Scaffold(
+  //     ),
+  //   );
+  // }
   @override
   Widget build(BuildContext context) {
-    width = MediaQuery.of(context).size.width;
-    return SafeArea(
-      child: Scaffold(
-      ),
+    return Material(
+        child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                      "MM YQF Hyper Garage",
+                      style: TextStyle(color: Colors.deepPurple, fontSize: 30)
+
+                  ),
+                  Image.asset("images/welcome2.jpg"),
+                  SizedBox(height: 30.0,),
+                  FlatButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/newpost');
+                      },
+                      child: Text(
+                          "New Post"
+                      )
+                  )
+                ]
+            )
+        )
     );
   }
 }
-
 
 
 Widget sourceInfo(ItemModel model, BuildContext context,
